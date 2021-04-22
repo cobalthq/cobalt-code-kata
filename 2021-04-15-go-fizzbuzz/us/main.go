042f2d9e3ec3821c42016e9dd3e07e0f7b221a69
Sergey Kruk <sergey@cobalt.io>
2021-04-22 19:56:34 +0200

    US time zone code kata complete

--- 2021-04-15-go-fizzbuzz/us/main.go
+++ 2021-04-15-go-fizzbuzz/us/main.go
@@ -1,5 +1,16 @@
 package main
 
-func FizzBuzz(num uint) string {
-	return ""
+import "strconv"
+
+func FizzBuzz(num uint64) (result string) {
+	if num%3 == 0 {
+		result += "Fizz"
+	}
+	if num%5 == 0 {
+		result += "Buzz"
+	}
+	if len(result) == 0 {
+		return strconv.FormatUint(num, 10)
+	}
+	return
 }
--- 2021-04-15-go-fizzbuzz/us/main_test.go
+++ 2021-04-15-go-fizzbuzz/us/main_test.go
@@ -1,7 +1,44 @@
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+	"testing/quick"
+)
 
-func TestFizzBuzz(t *testing.T) {
-	t.Errorf("Implement me")
+func TestFizzBuzz_map(t *testing.T) {
+	assertionMap := map[uint64]string{
+		1:  "1",
+		2:  "2",
+		3:  "Fizz",
+		5:  "Buzz",
+		6:  "Fizz",
+		10: "Buzz",
+		15: "FizzBuzz",
+		30: "FizzBuzz",
+	}
+	for input, output := range assertionMap {
+		result := FizzBuzz(input)
+		if result != output {
+			t.Errorf("Expecting FizzBuzz(%d) to be %#v but it's %#v", input, output, result)
+		}
+	}
+}
+
+func TestFizzBuzz_propFizz(t *testing.T) {
+	property := func(num uint16) bool {
+		return strings.HasPrefix(FizzBuzz(uint64(num)*3), "Fizz")
+	}
+	if err := quick.Check(property, nil); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestFizzBuzz_propBuzz(t *testing.T) {
+	property := func(num uint8) bool {
+		return strings.HasSuffix(FizzBuzz(uint64(num)*5), "Buzz")
+	}
+	if err := quick.Check(property, nil); err != nil {
+		t.Error(err)
+	}
 }
