0727846d73c76c6426f338cc89bf621ee4819ce4
Sergey Kruk <sergey@cobalt.io>
2021-09-09 12:07:47 +0200

    Implement leap year with EU

--- 2021-09-09-kotlin-leap-year/eu/src/main/kotlin/io/cobalt/codekata/leapyear/main.kt
+++ 2021-09-09-kotlin-leap-year/eu/src/main/kotlin/io/cobalt/codekata/leapyear/main.kt
@@ -1,5 +1,12 @@
 package io.cobalt.codekata.leapyear
 
-fun leapYear(year: Int): Boolean {
-    return false
-}
+// 1. All years divisible by 400 ARE leap years (so, for example, 2000 was indeed a leap year),
+// 2. All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year),
+// 3. All years divisible by 4 but not by 100 ARE leap years (e.g., 2008, 2012, 2016),
+fun leapYear(year: Int): Boolean =
+    when {
+        year % 400 == 0 -> true
+        year % 100 == 0 -> false
+        year % 4 == 0 -> true
+        else -> false
+    }
--- 2021-09-09-kotlin-leap-year/eu/src/test/kotlin/io/cobalt/codekata/leapyear/MainTest.kt
+++ 2021-09-09-kotlin-leap-year/eu/src/test/kotlin/io/cobalt/codekata/leapyear/MainTest.kt
@@ -2,10 +2,46 @@ package io.cobalt.codekata.leapyear
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import kotlin.random.Random
 
 class MainTest {
+    private val seed = Random(123).nextInt()
+    private val subSeed = Random(234).nextInt(1,24)
+    private val table = mapOf(
+        Pair(1, false),
+        Pair(2, false),
+        Pair(4, true),
+        Pair(100, false),
+        Pair(400, true),
+        Pair(500, false),
+        Pair(404, true)
+    )
+
     @Test
-    fun `implement me`() {
-        Assertions.assertTrue(leapYear(1000))
+    fun `leap year if dividable by 400`() {
+        Assertions.assertTrue(leapYear(seed * 400))
+    }
+
+    @Test
+    fun `1 is not a leap year`() {
+        Assertions.assertFalse(leapYear(seed * 4 + 1))
+    }
+
+    @Test
+    fun `divisible by 100 but not by 400 are NOT leap years`() {
+        Assertions.assertFalse(leapYear(seed * 400 + 100))
+    }
+
+    @Test
+    fun `divisible by 4 but not by 100 ARE leap years`() {
+        Assertions.assertTrue(leapYear(4))
+        Assertions.assertTrue(leapYear(8))
+        Assertions.assertTrue(leapYear(100 * seed + 4))
+        Assertions.assertTrue(leapYear(100 * seed + 4 * subSeed))
+    }
+
+    @Test
+    fun `table passes`() {
+        Assertions.assertTrue(table.all { leapYear(it.key) == it.value })
     }
 }
