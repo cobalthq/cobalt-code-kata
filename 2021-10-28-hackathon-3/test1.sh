b1aab3e80f09386eae6541916ffe62201e41fa2f
Sergey Kruk <sergey@cobalt.io>
2021-10-29 12:20:49 +0200

    Implement buildable version with ruby-kotlin-js-ruby flow

---
+++ 2021-10-28-hackathon-3/test1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p test
+cp main1.rb test/1.rb
+ruby test/1.rb > test/2.kt
+kotlinc test/2.kt -include-runtime -d test/2.jar && java -jar test/2.jar > test/3.js
+node test/3.js > test/4.rb
+diff test/1.rb test/4.rb
