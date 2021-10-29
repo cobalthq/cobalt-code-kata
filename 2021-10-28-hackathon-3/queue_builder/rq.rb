e6f4375cb4b4b74baffaf2dc5f881501dc2de349
Sergey Kruk <sergey@cobalt.io>
2021-10-29 14:25:38 +0200

    Optimize quote and endline management to only replace once

--- 2021-10-28-hackathon-3/queue_builder/rq.rb
+++ 2021-10-28-hackathon-3/queue_builder/rq.rb
@@ -3,6 +3,6 @@ eval r=<<-'RUBY'
 slf=%Q(queue="QQQ"
 eval r=<<-'RUBY'
 #{r}RUBY)
-next_quine,next_queue=(queue+":#{slf.gsub(10.chr,110.chr*2).gsub(34.chr,113.chr*2).gsub(':',99.chr*2).sub('QQQ','queue'.upcase)}").split(':',2)
-puts next_quine.gsub(110.chr*2,10.chr).gsub(113.chr*2,34.chr).gsub(99.chr*2,':').sub('queue'.upcase,next_queue)
+next_quine,next_queue=(queue+":#{slf.gsub(':',99.chr*2).sub('QQQ','queue'.upcase)}").split(':',2)
+puts next_quine.gsub(99.chr*2,':').sub('queue'.upcase,next_queue.gsub(10.chr,92.chr+110.chr).gsub(34.chr,92.chr+34.chr))
 RUBY
--- 2021-10-28-hackathon-3/test1.sh
+++ 2021-10-28-hackathon-3/test1.sh
@@ -2,6 +2,7 @@
 
 mkdir -p test
 cp main1.rb test/1.rb
+# ruby test/1.rb > test/3.js
 ruby test/1.rb > test/2.kt
 kotlinc test/2.kt -include-runtime -d test/2.jar && java -jar test/2.jar > test/3.js
 node test/3.js > test/4.rb
