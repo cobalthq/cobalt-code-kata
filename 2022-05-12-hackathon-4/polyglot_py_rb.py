c938ed5e9a1f219705eb68d9228a002ca01c075f
Sergey Kruk <sergey@cobalt.io>
2022-05-12 16:44:26 +0200

    [HACK-104] Add bilingual quine for python and ruby with steps

---
+++ 2022-05-12-hackathon-4/polyglot_py_rb.py
@@ -0,0 +1,1 @@
+s="s=%s;print(s %% (('%%c' %% 34) + s + ('%%c' %% 34)));0 and puts";print(s % (('%c' % 34) + s + ('%c' % 34)));0 and puts
--- 2022-05-12-hackathon-4/test.sh
+++ 2022-05-12-hackathon-4/test.sh
@@ -7,9 +7,22 @@ ruby polyglot_go_rb.go > test_output_rb
 diff polyglot_go_rb.go test_output_go
 diff polyglot_go_rb.go test_output_rb
 
+rm -rf test_*
+
 # Kotlin + Ruby
 
 kotlinc polyglot_kt_rb.kt -include-runtime -d polyglot.jar && java -jar polyglot.jar > test_output_kt
 ruby polyglot_kt_rb.kt > test_output_rb
 diff polyglot_kt_rb.kt test_output_kt
 diff polyglot_kt_rb.kt test_output_rb
+
+rm -rf test_*
+
+# Python + Ruby
+
+python polyglot_py_rb.py > test_output_py
+ruby polyglot_py_rb.py > test_output_rb
+diff polyglot_py_rb.py test_output_py
+diff polyglot_py_rb.py test_output_rb
+
+rm -rf test_*
