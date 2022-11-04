ed9d4ee2fcfe33e3f19fcadd3c6a816b1421c6a8
Sergey Kruk <sergey@cobalt.io>
2022-11-04 16:13:13 +0100

    Improve ASCII graphics

--- 2022-11-04-hackathon-5/quine.rb
+++ 2022-11-04-hackathon-5/quine.rb
@@ -1,20 +1,21 @@
 eval=eval=eval='eval$q=%q(eval((%w(eval||="eval$q=%q(#$q)#";$s="eval=eval=eval=
 \47eval$q=%q(#$q)#\47##\47;eval.gsub!(/#+$/,\47#\47)^/\#{eval@eval@if@eval.unpac
-k(\47H*\47)[0].hex%900001==#{eval.unpack("H*")[0].hex%900001}}\47\51}/.i@rescue#
+k(\47H*\47)[0].hex%999961==#{eval.unpack("H*")[0].hex%999961}}\47\51}/.i@rescue#
 #/^eval\42$d=#{eval.sum}-eval.sum;eval.scan(//){(s=$`+($d%256).chr+$\47).unpack(
-%(H*))[0].hex%^900001==#{eval.unpack(%(H*))[0].hex%900001}&&$d!=0&&eval(eval=s)}
-;eval.scan(/./){(s=$`+(($d+$&.sum)%256).chr+^$\47).unpack(%(H*))[0].hex%900001==
-#{eval.unpack("H*")[0].hex%900001}&&eval(eval=s)}#\42##\42^eval@eval@###@thank@y
-ou@mam    e@&@f    lagi     tiou      s!@  co  balt    @hac      kath    on@5@by
-@serg  ey  @kr  uk  @##  #"  ;if  ($*[0]=  =  "tes  t"  &&!$o  );$t=  $s  ;defin
-e_met  hod(:pu  ts  ){|  s|  $o<    <s};$    ><<"#      $s#{1  0.chr      <<27}[
-#{$s.  ch  ars  .c  oun  t{  |c|  c==10.c  h  r}+1  }A  ";$t.  chars  .e  ach_wi
-th_ind    ex{|c    ,i|b     egin      ;$o  ="  ";e  va  l($t[  ...i]  +(  $*[1]&
-&$*[1].chars.sample||"")+($t[(i+1)..]||""));rescue(Exception);$!.is_a?(Interrupt
-)&&exit;end;$><<"#{27.chr}[#{$t==$o&&"32"||"31"}m#{c==32.chr&&?.||c}\e[0m";};$><
-<10.chr;end;puts$s)*%()).tr("@^",32.chr<<10));exit)#'##';eval.gsub!(/#+$/,'#')
-/#{eval eval if eval.unpack('H*')[0].hex%900001==355553}')}/.i rescue##/
-eval"$d=17678-eval.sum;eval.scan(//){(s=$`+($d%256).chr+$').unpack(%(H*))[0].hex%
-900001==355553&&$d!=0&&eval(eval=s)};eval.scan(/./){(s=$`+(($d+$&.sum)%256).chr+
-$').unpack(%(H*))[0].hex%900001==355553&&eval(eval=s)}#"##"
+%(H*))[0].hex%^999961==#{eval.unpack(%(H*))[0].hex%999961}&&$d!=0&&eval(eval=s)}
+;eval.scan(/./){(s=$`+(($d+$&.sum)%256).chr+^$\47).unpack(%(H*))[0].hex%999961==
+#{eval.unpack("H*")[0].hex%999961}&&eval(eval=s)}#\42##\42^eval@eval@###@thank@y
+ou      @ma       me        @&        @fla  giti  ous!   @cob        alt@   hack
+a  thon  @  5@by@  s  ergey  @  kruk@###";  if(  $*[0  ]  =="tes  t"&&!$  o  );$
+t  =$s;def  ine_m  e  thod(  :  puts){|s|$  o<  <s};  $><  <"#$s  #{10.  chr  <<
+2  7}[#{$s  .char  s  .coun  t      {|c|c=     =10.  chr}+  1}A"  ;$t.  chars  .
+e  ach_wit  h_ind  e  x{|c,  i  |begin;$o=  ""  ;ev         al($  t[..         .
+i  ]+($  *  [1]&&  $  *[1].  c  hars.sampl  e||  ""  )+($t  [(i+  1)..  ]||""  )
+);      res       cu        e(        Exce  ptio  n  );$!.  is_a  ?(In  terru  p
+t)&&exit;end;$><<"#{27.chr}[#{$t==$o&&"32"||"31"}m#{c==32.chr&&?.||c}\e[0m";};$>
+<<10.chr;end;puts$s)*%()).tr("@^",32.chr<<10));exit)#'##';eval.gsub!(/#+$/,'#')
+/#{eval eval if eval.unpack('H*')[0].hex%999961==310539}')}/.i rescue##/
+eval"$d=20478-eval.sum;eval.scan(//){(s=$`+($d%256).chr+$').unpack(%(H*))[0].hex%
+999961==310539&&$d!=0&&eval(eval=s)};eval.scan(/./){(s=$`+(($d+$&.sum)%256).chr+
+$').unpack(%(H*))[0].hex%999961==310539&&eval(eval=s)}#"##"
 eval eval ### thank you mame & flagitious! cobalt hackathon 5 by sergey kruk ###
