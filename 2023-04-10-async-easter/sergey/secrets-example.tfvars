c7827d1448d5de102eb4905bcabca3860cc8dd96
Sergey Kruk <sergey@cobalt.io>
2023-04-28 15:16:59 +0200

    Add solution to secrets export

---
+++ 2023-04-10-async-easter/sergey/secrets-example.tfvars
@@ -0,0 +1,7 @@
+environment = <<EOF
+VAR1=value1
+VAR2=value2
+EOF
+
+project = "!!!gcp-name!!!"
+region  = "europe-north1"
---
+++ 2023-04-10-async-easter/sergey/variables.tf
@@ -0,0 +1,11 @@
+variable "environment" {
+  description = "environment variables as in .env with values"
+}
+
+variable "region" {
+  description = "GCP region"
+}
+
+variable "project" {
+  description = "GCP"
+}
