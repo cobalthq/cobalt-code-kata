f6349bcce28b7f74f02a23e4607bd8c909501754
Sergey Kruk <sergey@cobalt.io>
2023-07-31 12:43:05 +0200

    Implement shunting yard in EU time zone

--- 2023-07-03-ruby-regex/eu/spec/regex_engine_spec.rb
+++ 2023-07-03-ruby-regex/eu/spec/regex_engine_spec.rb
@@ -3,23 +3,63 @@ require "regex_engine"
 RSpec.describe RegexEngine do
   subject(:engine) { described_class.new }
 
-  it 'prepares "ab" as "a+b"' do
-    expect(engine.prepare("ab")).to eq("a+b")
+  describe '#prepare' do
+    it 'prepares "ab" as "a+b"' do
+      expect(engine.prepare("ab")).to eq("a+b")
+    end
+
+    it 'prepares "ab|c" as "a+b|c"' do
+      expect(engine.prepare("ab|c")).to eq("a+b|c")
+    end
+
+    it 'prepares "ab|c(d)" as "a+b|c+(d)"' do
+      expect(engine.prepare("ab|c(d)")).to eq("a+b|c+(d)")
+    end
+
+    it 'prepares "ab|c(d|e)*" as "a+b|c+(d|e)*"' do
+      expect(engine.prepare("ab|c(d|e)*")).to eq("a+b|c+(d|e)*")
+    end
+
+    it 'prepares "ab*|c(d|e)*f" as "a+b*|c+(d|e)*+f"' do
+      expect(engine.prepare("ab*|c(d|e)*f")).to eq("a+b*|c+(d|e)*+f")
+    end
   end
 
-  it 'prepares "ab|c" as "a+b|c"' do
-    expect(engine.prepare("ab|c")).to eq("a+b|c")
-  end
+  describe '#shunting_yard' do
+    it 'transforms "a+b" to "ab+"' do
+      expect(engine.shunting_yard('a+b')).to eq('ab+')
+    end
 
-  it 'prepares "ab|c(d)" as "a+b|c+(d)"' do
-    expect(engine.prepare("ab|c(d)")).to eq("a+b|c+(d)")
-  end
+    it 'transforms "a+b+c" to "ab+c+"' do
+      expect(engine.shunting_yard('a+b+c')).to eq('ab+c+')
+    end
 
-  it 'prepares "ab|c(d|e)*" as "a+b|c+(d|e)*"' do
-    expect(engine.prepare("ab|c(d|e)*")).to eq("a+b|c+(d|e)*")
-  end
+    it 'transforms "a|b+c" to "abc+|"' do
+      expect(engine.shunting_yard('a|b+c')).to eq('abc+|')
+    end
 
-  it 'prepares "ab*|c(d|e)*f" as "a+b*|c+(d|e)*+f"' do
-    expect(engine.prepare("ab*|c(d|e)*f")).to eq("a+b*|c+(d|e)*+f")
+    it 'transforms "a|b+c" to "abc+|"' do
+      expect(engine.shunting_yard('a|b+c')).to eq('abc+|')
+    end
+
+    it 'transforms "a|b+c+d|f" to "abc+d+|f|"' do
+      expect(engine.shunting_yard('a|b+c+d|f')).to eq('abc+d+|f|')
+    end
+
+    it 'transforms "a|b*+c" to "ab*c+|"' do
+      expect(engine.shunting_yard('a|b*+c')).to eq('ab*c+|')
+    end
+
+    it 'transforms "a|(b+c)*" to "abc+*|"' do
+      expect(engine.shunting_yard('a|(b+c)*')).to eq('abc+*|')
+    end
+
+    it 'transforms "a|(b|c+d)*" to "abcd+|*|"' do
+      expect(engine.shunting_yard('a|(b|c+d)*')).to eq('abcd+|*|')
+    end
+
+    it 'transforms "a+b|c+(d|e)*+f" to "ab+cde|*+f+|"' do
+      expect(engine.shunting_yard('a+b|c+(d|e)*+f')).to eq('ab+cde|*+f+|')
+    end
   end
 end
